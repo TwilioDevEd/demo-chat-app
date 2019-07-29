a132d5816c386e19dedc99f22af730ab26a3f77e
Mica Swyers <mswyers@twilio.com>
2019-07-29 11:53:00 -0700

    Run Prettier on all js files; remove serviceWorker

--- src/ChatChannel.js
+++ src/ChatChannel.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import "./Chat.css";
-import MessageBubble from "./MessageBubble";
-import Dropzone from "react-dropzone";
-import styles from "./ChatChannel.module.css";
+import React, {Component} from 'react';
+import './Chat.css';
+import MessageBubble from './MessageBubble';
+import Dropzone from 'react-dropzone';
+import styles from './ChatChannel.module.css';
 
 class ChatChannel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newMessage: "",
+      newMessage: '',
       messages: [],
-      loadingState: "initializing",
-      boundChannels: new Set()
+      loadingState: 'initializing',
+      boundChannels: new Set(),
     };
   }
 
@@ -23,13 +23,13 @@ class ChatChannel extends Component {
           if (this.props.channelProxy === thisChannel) {
             this.setState({
               messages: messagePaginator.items,
-              loadingState: "ready"
+              loadingState: 'ready',
             });
           }
         })
         .catch(err => {
           console.error("Couldn't fetch messages IMPLEMENT RETRY", err);
-          this.setState({ loadingState: "failed" });
+          this.setState({loadingState: 'failed'});
         });
     }
   };
@@ -40,9 +40,9 @@ class ChatChannel extends Component {
 
       if (!this.state.boundChannels.has(this.props.channelProxy)) {
         let newChannel = this.props.channelProxy;
-        newChannel.on("messageAdded", m => this.messageAdded(m, newChannel));
+        newChannel.on('messageAdded', m => this.messageAdded(m, newChannel));
         this.setState({
-          boundChannels: new Set([...this.state.boundChannels, newChannel])
+          boundChannels: new Set([...this.state.boundChannels, newChannel]),
         });
       }
     }
@@ -54,9 +54,9 @@ class ChatChannel extends Component {
 
       if (!this.state.boundChannels.has(this.props.channelProxy)) {
         let newChannel = this.props.channelProxy;
-        newChannel.on("messageAdded", m => this.messageAdded(m, newChannel));
+        newChannel.on('messageAdded', m => this.messageAdded(m, newChannel));
         this.setState({
-          boundChannels: new Set([...this.state.boundChannels, newChannel])
+          boundChannels: new Set([...this.state.boundChannels, newChannel]),
         });
       }
     }
@@ -64,13 +64,13 @@ class ChatChannel extends Component {
 
   static getDerivedStateFromProps(newProps, oldState) {
     let logic =
-      oldState.loadingState === "initializing" ||
+      oldState.loadingState === 'initializing' ||
       oldState.channelProxy !== newProps.channelProxy;
-    console.log("xxx", oldState.channelProxy, newProps.channelProxy, logic);
+    console.log('xxx', oldState.channelProxy, newProps.channelProxy, logic);
     if (logic) {
       return {
-        loadingState: "loading messages",
-        channelProxy: newProps.channelProxy
+        loadingState: 'loading messages',
+        channelProxy: newProps.channelProxy,
       };
     } else {
       return null;
@@ -80,18 +80,18 @@ class ChatChannel extends Component {
   messageAdded = (message, targetChannel) => {
     if (targetChannel === this.props.channelProxy)
       this.setState((prevState, props) => ({
-        messages: [...prevState.messages, message]
+        messages: [...prevState.messages, message],
       }));
   };
 
   onMessageChanged = event => {
-    this.setState({ newMessage: event.target.value });
+    this.setState({newMessage: event.target.value});
   };
 
   sendMessage = event => {
     event.preventDefault();
     const message = this.state.newMessage;
-    this.setState({ newMessage: "" });
+    this.setState({newMessage: ''});
     this.props.channelProxy.sendMessage(message);
   };
 
@@ -99,7 +99,7 @@ class ChatChannel extends Component {
     console.log(acceptedFiles);
     this.props.channelProxy.sendMessage({
       contentType: acceptedFiles[0].type,
-      media: acceptedFiles[0]
+      media: acceptedFiles[0],
     });
   };
 
@@ -123,22 +123,21 @@ class ChatChannel extends Component {
           <input
             type="text"
             name="message"
-            id={styles["type-a-message"]}
+            id={styles['type-a-message']}
             autocomplete="off"
-            disabled={this.state.loadingState !== "ready"}
+            disabled={this.state.loadingState !== 'ready'}
             onChange={this.onMessageChanged}
             value={this.state.newMessage}
           />
           <button>Send</button>
         </form>
         <Dropzone onDrop={this.onDrop} accept="image/*">
-          {({ getRootProps, getInputProps, isDragActive }) => (
+          {({getRootProps, getInputProps, isDragActive}) => (
             <div
               {...getRootProps()}
               className={`${styles.Dropzone} ${
-                isDragActive ? styles.Highlight : ""
-              }`}
-            >
+                isDragActive ? styles.Highlight : ''
+              }`}>
               <input id="files" {...getInputProps()} />
               <p>Drag Files Here</p>
             </div>
