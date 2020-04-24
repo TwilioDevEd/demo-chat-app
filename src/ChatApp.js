021c2fd71a141cf5c2a1a45101a61fc2534c4019
AnNguyen12345 <42226726+AnNguyen12345@users.noreply.github.com>
2020-04-24 06:54:32 -0500

    Add my token

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import NameBox from './NameBox.js';
-import {Client as ChatClient} from 'twilio-chat';
-import ChatChannel from './ChatChannel';
-import './Chat.css';
+import React from "react";
+import NameBox from "./NameBox.js";
+import { Client as ChatClient } from "twilio-chat";
+import ChatChannel from "./ChatChannel";
+import "./Chat.css";
 import {
   BrowserRouter as Router,
   NavLink,
   Route,
-  Redirect,
-} from 'react-router-dom';
+  Redirect
+} from "react-router-dom";
 
 class ChatApp extends React.Component {
   constructor(props) {
     super(props);
-    const name = localStorage.getItem('name') || '';
-    const loggedIn = name !== '';
+    const name = localStorage.getItem("name") || "";
+    const loggedIn = name !== "";
     this.state = {
       name,
       loggedIn,
@@ -23,87 +23,88 @@ class ChatApp extends React.Component {
       chatReady: false,
       channels: [],
       selectedChannel: null,
-      newMessage: '',
+      newMessage: ""
     };
-    this.channelName = 'general';
+    this.channelName = "general";
   }
 
   componentWillMount = () => {
     if (this.state.loggedIn) {
       this.getToken();
-      this.setState({statusString: 'Fetching credentials…'});
+      this.setState({ statusString: "Fetching credentials…" });
     }
   };
 
   onNameChanged = event => {
-    this.setState({name: event.target.value});
+    this.setState({ name: event.target.value });
   };
 
   logIn = event => {
     event.preventDefault();
-    if (this.state.name !== '') {
-      localStorage.setItem('name', this.state.name);
-      this.setState({loggedIn: true}, this.getToken);
+    if (this.state.name !== "") {
+      localStorage.setItem("name", this.state.name);
+      this.setState({ loggedIn: true }, this.getToken);
     }
   };
 
   logOut = event => {
     event.preventDefault();
     this.setState({
-      name: '',
+      name: "",
       loggedIn: false,
-      token: '',
+      token: "",
       chatReady: false,
       messages: [],
-      newMessage: '',
-      channels: [],
+      newMessage: "",
+      channels: []
     });
-    localStorage.removeItem('name');
+    localStorage.removeItem("name");
     this.chatClient.shutdown();
     this.channel = null;
   };
 
   getToken = () => {
     // Paste your unique Chat token function
-    const myToken = '<Your token here>';
-    this.setState({token: myToken}, this.initChat);
+    const myToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzVlNWFhNTExZTMxMDU1MDk4NTc1Y2E4Y2MxNmM1N2ZkLTE1ODc3MjUyNDEiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJBbjEiLCJjaGF0Ijp7InNlcnZpY2Vfc2lkIjoiSVMwZWU3YWJmZTc4NzE0ZGVhYWQ3YzlhMjhjODEzZDRiNyJ9fSwiaWF0IjoxNTg3NzI1MjQxLCJleHAiOjE1ODc3Mjg4NDEsImlzcyI6IlNLNWU1YWE1MTFlMzEwNTUwOTg1NzVjYThjYzE2YzU3ZmQiLCJzdWIiOiJBQ2MwMzhkMWIwYjA2NjhmN2NjMmQzY2M2OWVkNjgyNzFlIn0.ZV0xIJ5rDcA06Hfz0iYeS0rK8yiQtB-oEjHRBLHwTII";
+    this.setState({ token: myToken }, this.initChat);
   };
 
   initChat = async () => {
     window.chatClient = ChatClient;
     this.chatClient = await ChatClient.create(this.state.token, {
-      logLevel: 'info',
+      logLevel: "info"
     });
-    this.setState({statusString: 'Connecting to Twilio…'});
+    this.setState({ statusString: "Connecting to Twilio…" });
 
-    this.chatClient.on('connectionStateChanged', state => {
-      if (state === 'connecting')
-        this.setState({statusString: 'Connecting to Twilio…'});
-      if (state === 'connected') {
-        this.setState({statusString: 'You are connected.'});
+    this.chatClient.on("connectionStateChanged", state => {
+      if (state === "connecting")
+        this.setState({ statusString: "Connecting to Twilio…" });
+      if (state === "connected") {
+        this.setState({ statusString: "You are connected." });
       }
-      if (state === 'disconnecting')
+      if (state === "disconnecting")
         this.setState({
-          statusString: 'Disconnecting from Twilio…',
-          chatReady: false,
+          statusString: "Disconnecting from Twilio…",
+          chatReady: false
         });
-      if (state === 'disconnected')
-        this.setState({statusString: 'Disconnected.', chatReady: false});
-      if (state === 'denied')
-        this.setState({statusString: 'Failed to connect.', chatReady: false});
+      if (state === "disconnected")
+        this.setState({ statusString: "Disconnected.", chatReady: false });
+      if (state === "denied")
+        this.setState({ statusString: "Failed to connect.", chatReady: false });
     });
-    this.chatClient.on('channelJoined', channel => {
-      this.setState({channels: [...this.state.channels, channel]});
+    this.chatClient.on("channelJoined", channel => {
+      this.setState({ channels: [...this.state.channels, channel] });
     });
-    this.chatClient.on('channelLeft', thisChannel => {
+    this.chatClient.on("channelLeft", thisChannel => {
       this.setState({
-        channels: [...this.state.channels.filter(it => it !== thisChannel)],
+        channels: [...this.state.channels.filter(it => it !== thisChannel)]
       });
     });
   };
 
   messagesLoaded = messagePage => {
-    this.setState({messages: messagePage.items});
+    this.setState({ messages: messagePage.items });
   };
 
   render() {
@@ -123,7 +124,8 @@ class ChatApp extends React.Component {
                         key={channel.sid}
                         to={`/channels/${channel.sid}`}
                         className="list-group-item list-group-item-action"
-                        activeClassName="active">
+                        activeClassName="active"
+                      >
                         <li>{channel.friendlyName}</li>
                       </NavLink>
                     ))}
@@ -133,10 +135,10 @@ class ChatApp extends React.Component {
                 <div id="SelectedChannel" className="col-lg">
                   <Route
                     path="/channels/:selected_channel"
-                    render={({match}) => {
+                    render={({ match }) => {
                       let selectedChannelSid = match.params.selected_channel;
                       let selectedChannel = this.state.channels.find(
-                        it => it.sid === selectedChannelSid,
+                        it => it.sid === selectedChannelSid
                       );
                       if (selectedChannel)
                         return (
